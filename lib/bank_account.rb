d02dd1e237056bff79c8c1e6f3bffe138203c6d5
Noor Sulaiman <noor.s.suliman@gmail.com>
2017-01-08 21:42:49 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,25 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  def deposit(bal)
+    self.balance += bal.to_i
+  end
+  def display_balance
+     "Your balance is $#{@balance}."
+  end
+  def valid?
+    if self.status == "open" && self.balance > 0
+      return true
+    else
+      false
+    end
+  end
+  def close_account
+    self.status = "closed"
+  end
 end
